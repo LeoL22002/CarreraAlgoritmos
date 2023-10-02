7fe70bb53ab34a50bd3df143e3615610a323abd0
Leo Lorenzo <95876108+LeoL22002@users.noreply.github.com>
2023-10-01 22:19:16 -0400

    Agregar archivos de proyecto.

---
+++ CarreraAlgoritmos/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace CarreraAlgoritmos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.secuencialBox = new System.Windows.Forms.RichTextBox();
+            this.BinariaBox = new System.Windows.Forms.RichTextBox();
+            this.BurbujaBox = new System.Windows.Forms.RichTextBox();
+            this.QuiksortBox = new System.Windows.Forms.RichTextBox();
+            this.InsercionBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ArregloBox = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.labelres = new System.Windows.Forms.Label();
+            this.labelSecuencial = new System.Windows.Forms.Label();
+            this.labelBinaria = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.labelBurbuja = new System.Windows.Forms.Label();
+            this.labelQuik = new System.Windows.Forms.Label();
+            this.labelInsercion = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buscartxt = new System.Windows.Forms.TextBox();
+            this.t_memoria = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(996, 562);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(996, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Limpiar Datos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // secuencialBox
+            // 
+            this.secuencialBox.Location = new System.Drawing.Point(319, 70);
+            this.secuencialBox.Name = "secuencialBox";
+            this.secuencialBox.Size = new System.Drawing.Size(213, 247);
+            this.secuencialBox.TabIndex = 2;
+            this.secuencialBox.Text = "";
+            // 
+            // BinariaBox
+            // 
+            this.BinariaBox.Location = new System.Drawing.Point(597, 70);
+            this.BinariaBox.Name = "BinariaBox";
+            this.BinariaBox.Size = new System.Drawing.Size(213, 247);
+            this.BinariaBox.TabIndex = 3;
+            this.BinariaBox.Text = "";
+            // 
+            // BurbujaBox
+            // 
+            this.BurbujaBox.Location = new System.Drawing.Point(12, 369);
+            this.BurbujaBox.Name = "BurbujaBox";
+            this.BurbujaBox.Size = new System.Drawing.Size(213, 247);
+            this.BurbujaBox.TabIndex = 4;
+            this.BurbujaBox.Text = "";
+            // 
+            // QuiksortBox
+            // 
+            this.QuiksortBox.Location = new System.Drawing.Point(319, 369);
+            this.QuiksortBox.Name = "QuiksortBox";
+            this.QuiksortBox.Size = new System.Drawing.Size(213, 247);
+            this.QuiksortBox.TabIndex = 5;
+            this.QuiksortBox.Text = "";
+            // 
+            // InsercionBox
+            // 
+            this.InsercionBox.Location = new System.Drawing.Point(595, 369);
+            this.InsercionBox.Name = "InsercionBox";
+            this.InsercionBox.Size = new System.Drawing.Size(213, 247);
+            this.InsercionBox.TabIndex = 6;
+            this.InsercionBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Busqueda Secuencial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(591, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Busqueda Binaria";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Metodo de Burbuja";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(315, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "QuikSort";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(591, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(252, 31);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Metodo de Insercion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 31);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Arreglo Original";
+            // 
+            // ArregloBox
+            // 
+            this.ArregloBox.Location = new System.Drawing.Point(12, 70);
+            this.ArregloBox.Name = "ArregloBox";
+            this.ArregloBox.Size = new System.Drawing.Size(213, 247);
+            this.ArregloBox.TabIndex = 12;
+            this.ArregloBox.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(996, 472);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 57);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Generar Datos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelres
+            // 
+            this.labelres.AutoSize = true;
+            this.labelres.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelres.Location = new System.Drawing.Point(6, 662);
+            this.labelres.Name = "labelres";
+            this.labelres.Size = new System.Drawing.Size(284, 31);
+            this.labelres.TabIndex = 15;
+            this.labelres.Text = "RESULTADOS FINALES";
+            // 
+            // labelSecuencial
+            // 
+            this.labelSecuencial.AutoSize = true;
+            this.labelSecuencial.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSecuencial.Location = new System.Drawing.Point(6, 708);
+            this.labelSecuencial.Name = "labelSecuencial";
+            this.labelSecuencial.Size = new System.Drawing.Size(255, 31);
+            this.labelSecuencial.TabIndex = 16;
+            this.labelSecuencial.Text = "Busqueda Secuencial";
+            this.labelSecuencial.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // labelBinaria
+            // 
+            this.labelBinaria.AutoSize = true;
+            this.labelBinaria.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBinaria.Location = new System.Drawing.Point(6, 746);
+            this.labelBinaria.Name = "labelBinaria";
+            this.labelBinaria.Size = new System.Drawing.Size(211, 31);
+            this.labelBinaria.TabIndex = 17;
+            this.labelBinaria.Text = "Busqueda Binaria";
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(1051, 769);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(8, 8);
+            this.vScrollBar1.TabIndex = 18;
+            // 
+            // labelBurbuja
+            // 
+            this.labelBurbuja.AutoSize = true;
+            this.labelBurbuja.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBurbuja.Location = new System.Drawing.Point(6, 789);
+            this.labelBurbuja.Name = "labelBurbuja";
+            this.labelBurbuja.Size = new System.Drawing.Size(236, 31);
+            this.labelBurbuja.TabIndex = 19;
+            this.labelBurbuja.Text = "Metodo de Burbuja";
+            // 
+            // labelQuik
+            // 
+            this.labelQuik.AutoSize = true;
+            this.labelQuik.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuik.Location = new System.Drawing.Point(6, 831);
+            this.labelQuik.Name = "labelQuik";
+            this.labelQuik.Size = new System.Drawing.Size(115, 31);
+            this.labelQuik.TabIndex = 20;
+            this.labelQuik.Text = "QuikSort";
+            // 
+            // labelInsercion
+            // 
+            this.labelInsercion.AutoSize = true;
+            this.labelInsercion.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInsercion.Location = new System.Drawing.Point(6, 871);
+            this.labelInsercion.Name = "labelInsercion";
+            this.labelInsercion.Size = new System.Drawing.Size(252, 31);
+            this.labelInsercion.TabIndex = 21;
+            this.labelInsercion.Text = "Metodo de Insercion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(873, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 31);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Buscar";
+            // 
+            // buscartxt
+            // 
+            this.buscartxt.Location = new System.Drawing.Point(968, 76);
+            this.buscartxt.Name = "buscartxt";
+            this.buscartxt.Size = new System.Drawing.Size(192, 26);
+            this.buscartxt.TabIndex = 23;
+            // 
+            // t_memoria
+            // 
+            this.t_memoria.AutoSize = true;
+            this.t_memoria.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t_memoria.Location = new System.Drawing.Point(6, 913);
+            this.t_memoria.Name = "t_memoria";
+            this.t_memoria.Size = new System.Drawing.Size(352, 31);
+            this.t_memoria.TabIndex = 24;
+            this.t_memoria.Text = "Total de memoria consumida:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1163, 953);
+            this.Controls.Add(this.t_memoria);
+            this.Controls.Add(this.buscartxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.labelInsercion);
+            this.Controls.Add(this.labelQuik);
+            this.Controls.Add(this.labelBurbuja);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.labelBinaria);
+            this.Controls.Add(this.labelSecuencial);
+            this.Controls.Add(this.labelres);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ArregloBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InsercionBox);
+            this.Controls.Add(this.QuiksortBox);
+            this.Controls.Add(this.BurbujaBox);
+            this.Controls.Add(this.BinariaBox);
+            this.Controls.Add(this.secuencialBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Competencia De Algoritmos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox secuencialBox;
+        private System.Windows.Forms.RichTextBox BinariaBox;
+        private System.Windows.Forms.RichTextBox BurbujaBox;
+        private System.Windows.Forms.RichTextBox QuiksortBox;
+        private System.Windows.Forms.RichTextBox InsercionBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox ArregloBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label labelres;
+        private System.Windows.Forms.Label labelSecuencial;
+        private System.Windows.Forms.Label labelBinaria;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label labelBurbuja;
+        private System.Windows.Forms.Label labelQuik;
+        private System.Windows.Forms.Label labelInsercion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox buscartxt;
+        private System.Windows.Forms.Label t_memoria;
+    }
+}
+
